0b2c0722b79331295b807f2390b2844f07163f27
Dima <dimaberry1@gmail.com>
2022-10-16 02:57:10 +0300

    feat: complete binary-search-tree

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
+
+  let current = this.head;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
